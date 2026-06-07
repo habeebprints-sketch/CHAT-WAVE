16917e720c3c7b55edd0c05cc2e261cc9568a7e6
habeebprints-sketch <habeebprints@gmail.com>
2026-06-07 14:49:40 +0100

    Update firebase_service.dart

--- lib/lib/lib/services/firebase_service.dart
+++ lib/lib/lib/services/firebase_service.dart
@@ -1,1 +1,40 @@
-class Temp {}
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirebaseService {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final FirebaseFirestore db = FirebaseFirestore.instance;
+
+  // AUTH
+  Future<User?> signUp(String email, String password) async {
+    final res = await auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return res.user;
+  }
+
+  Future<User?> login(String email, String password) async {
+    final res = await auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return res.user;
+  }
+
+  // CHAT
+  Future<void> sendMessage(String text, String userId) async {
+    await db.collection("messages").add({
+      "text": text,
+      "userId": userId,
+      "time": FieldValue.serverTimestamp(),
+    });
+  }
+
+  Stream<QuerySnapshot> getMessages() {
+    return db.collection("messages")
+        .orderBy("time", descending: false)
+        .snapshots();
+  }
+}
