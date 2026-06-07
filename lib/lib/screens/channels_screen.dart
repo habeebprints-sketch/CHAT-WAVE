aceafad7638a65c7b6f9b89c6842d80a4f8a640e
habeebprints-sketch <habeebprints@gmail.com>
2026-06-07 14:47:48 +0100

    Update channels_screen.dart

--- lib/lib/screens/channels_screen.dart
+++ lib/lib/screens/channels_screen.dart
@@ -1,1 +1,26 @@
-class Temp {}
+import 'package:flutter/material.dart';
+
+class ChannelsScreen extends StatelessWidget {
+  const ChannelsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Channels")),
+      body: ListView(
+        children: const [
+          ListTile(
+            leading: Icon(Icons.campaign),
+            title: Text("Tech News"),
+            subtitle: Text("Latest updates"),
+          ),
+          ListTile(
+            leading: Icon(Icons.campaign),
+            title: Text("School Updates"),
+            subtitle: Text("Announcements"),
+          ),
+        ],
+      ),
+    );
+  }
+}
