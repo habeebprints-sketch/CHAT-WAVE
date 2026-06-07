24106ab63a0374602cebfb728314ecde870a1ae8
habeebprints-sketch <habeebprints@gmail.com>
2026-06-07 14:42:29 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,23 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+import 'screens/chat_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LoginScreen(),
+        '/chat': (context) => const ChatScreen(),
+      },
+    );
+  }
+}
